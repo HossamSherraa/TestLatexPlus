33775f4de1ccb64aab57c8c4eccd19b24d23fe4c
HossamSherraa <54014933+HossamSherraa@users.noreply.github.com>
2023-12-24 02:20:01 +0200

    Initial Commit

---
+++ TestLatexPlus/TestLatexPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLatexPlusApp.swift
+//  TestLatexPlus
+//
+//  Created by macbook air on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestLatexPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
